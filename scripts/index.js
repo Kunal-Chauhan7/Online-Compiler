fbe8d32e7e156db8970aa4ebd454394f62ad6945
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-01-01 16:25:10 +0530

    added javascript funcnality in the editor

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+let editor;
+window.onload = function(){
+    editor = ace.edit("Editor");
+    editor.setTheme("ace/theme/dracula");
+    editor.session.setMode("ace/mode/c_cpp");
+}
+
+function changelanguage(){
+    let Language = $(languages).val();
+    if(Language === "C" || Language === "C++"){
+        editor.session.setMode("ace/mode/c_cpp");
+    }
+    else if(Language === "Python"){
+        editor.session.setMode("ace/mode/python");
+    }
+    else if(Language === "Java"){
+        editor.session.setMode("ace/mode/java");
+    }
+}
